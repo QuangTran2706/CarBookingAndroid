41780dea397f196174fbab0cf7358a232b2946a5
QuangTran2706 <s3757281@rmit.edu.vn>
2021-04-03 13:38:46 +0700

    first commit

---
+++ app/src/main/java/com/trungngo/carshareapp/ui/customer/home/CustomerHomeViewModel.java
@@ -0,0 +1,23 @@
+package com.trungngo.carshareapp.ui.customer.home;
+
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.trungngo.carshareapp.model.User;
+
+public class CustomerHomeViewModel extends ViewModel {
+    private MutableLiveData<User> currentUserObject;
+
+
+    public CustomerHomeViewModel() {
+        currentUserObject = new MutableLiveData<>();
+    }
+
+    public void setCurrentUserObject(User currentUserObject) {
+        this.currentUserObject.setValue(currentUserObject);
+    }
+
+    public MutableLiveData<User> getCurrentUserObject(){
+        return this.currentUserObject;
+    }
+}
